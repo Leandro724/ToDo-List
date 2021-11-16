00cfd2861856dccc64abae70f5fcc3d9f0c56814
Leandro724 <88081194+Leandro724@users.noreply.github.com>
2021-11-16 11:01:45 +0200

    Update JS
    Added a function that adds a task and displays in to the main interface once the Add button in the form is clicked

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,34 @@
 // Selectors
+const userInput = document.getElementById("userInput");
+const inputButton = document.getElementById("inputButton");
+const listItems = document.getElementById("listItems");
 
 // Even Listeners
+inputButton.addEventListener('click',addTask)
 
 // Functions
+
+function addTask (event){
+    //Prevent form from submitting
+    event.preventDefault();
+    // Task Div
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('task');
+    // Create List item
+    const newTask = document.createElement('li');
+    newTask.classList.add('task-item');
+    newTask.innerText = 'hello';
+    taskDiv.appendChild(newTask);
+    // Task Completed button
+    const taskCompleted = document.createElement('button');
+    taskCompleted.innerText = 'Done';
+    taskCompleted.classList.add('completed-btn');
+    taskDiv.appendChild(taskCompleted);
+    // Delete button
+    const taskDelete = document.createElement('button');
+    taskDelete.innerText = 'Delete';
+    taskDelete.classList.add('completed-btn');
+    taskDiv.appendChild(taskDelete);
+    // Append to List Items
+    listItems.appendChild(taskDiv);
+}
